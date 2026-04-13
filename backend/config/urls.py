ee035ed48329a25bdf23c0601329b536d085350e
AknietBarakhan <ekwon387@gmail.com>
2026-04-13 20:52:42 +0500

    akniet

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.contrib import admin
 from django.urls import path, include
+from .views import api_login, api_logout
 
 def home(request):
     return HttpResponse("Server working")
@@ -12,4 +13,6 @@ urlpatterns = [
     path('api/tasks/',include('tasks.urls')),
     path('api/notes/', include('notes.urls')),
     path('api/calendar/', include('calendar_app.urls')),
+    path('api/login/',api_login,name='api_login'),
+    path('api/logout/',api_logout,name='api_logout'),
 ]
