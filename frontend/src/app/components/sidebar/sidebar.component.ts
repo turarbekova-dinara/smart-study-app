172b3b956cfe5de54d3c1f4b45aa148c61d74773
Dinara <turarbekovadinara5@gmail.com>
2026-04-13 16:25:29 +0500

    page

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './sidebar.component.html'
 })
 export class SidebarComponent {
