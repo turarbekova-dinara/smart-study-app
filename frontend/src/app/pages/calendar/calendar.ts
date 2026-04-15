0e0d26e08c6216c5c706b9d4d70230dc8bcc9435
Dinara <turarbekovadinara5@gmail.com>
2026-04-15 11:32:24 +0500

    merge fix

---
+++ frontend/src/app/pages/calendar/calendar.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar',
+  standalone: true,
+  template: `<h2>Calendar works ✅</h2>`
+})
+export class CalendarComponent {}
---
+++ frontend/src/app/pages/notes/notes.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notes',
+  standalone: true,
+  template: `<h2>Notes works</h2>`
+})
+export class NotesComponent {}
