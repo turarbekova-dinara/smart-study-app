560dc3458471adc4da8ca0fd0706d32303a39bb8
Dinara <turarbekovadinara5@gmail.com>
2026-04-15 11:22:36 +0500

    Back-End

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from .views import TaskListCreateAPIView, TaskDetailAPIView
 from .views import login_view, register_view
 
 urlpatterns = [
+  path('tasks/', TaskListCreateAPIView.as_view()),
+  path('tasks/<int:id>/', TaskDetailAPIView.as_view()),
   path('login/', login_view),
   path('register/', register_view),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,37 +1,101 @@
-from django.http import JsonResponse
-from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Task
+from .serializers import TaskSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view
 from django.contrib.auth import authenticate
-import json
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+
+# 1. ALL TASKS + CREATE
+class TaskListCreateAPIView(APIView):
+  permission_classes = [IsAuthenticated]
+
+  def get(self, request):
+    # тек сол user-дің task-тарын алу
+    tasks = Task.objects.filter(user=request.user)
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+  def post(self, request):
+    serializer = TaskSerializer(data=request.data)
+
+    if serializer.is_valid():
+      # user автоматты түрде қосылады
+      serializer.save(user=request.user)
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+# 2. DETAIL / UPDATE / DELETE
+class TaskDetailAPIView(APIView):
+  permission_classes = [IsAuthenticated]
+
+  def get_object(self, id, user):
+    try:
+      return Task.objects.get(id=id, user=user)
+    except Task.DoesNotExist:
+      return None
+
+  def get(self, request, id):
+    task = self.get_object(id, request.user)
+    if not task:
+      return Response({'error': 'Not found'}, status=404)
+
+    serializer = TaskSerializer(task)
+    return Response(serializer.data)
+
+  def put(self, request, id):
+    task = self.get_object(id, request.user)
+    if not task:
+      return Response({'error': 'Not found'}, status=404)
+
+    serializer = TaskSerializer(task, data=request.data)
+    if serializer.is_valid():
+      serializer.save(user=request.user)
+      return Response(serializer.data)
+
+    return Response(serializer.errors, status=400)
+
+  def delete(self, request, id):
+    task = self.get_object(id, request.user)
+    if not task:
+      return Response({'error': 'Not found'}, status=404)
+
+    task.delete()
+    return Response({'status': 'deleted'})
+
+@api_view(['POST'])
 def login_view(request):
-  if request.method == 'POST':
-    data = json.loads(request.body)
+  username = request.data.get('username')
+  password = request.data.get('password')
 
-    username = data.get('username')
-    password = data.get('password')
+  user = authenticate(username=username, password=password)
 
-    user = authenticate(username=username, password=password)
+  if user is not None:
+    token, created = Token.objects.get_or_create(user=user)
 
-    if user is not None:
-      return JsonResponse({'status': 'ok'})
-    else:
-      return JsonResponse({'status': 'error'})
+    return Response({
+      'token': token.key
+    })
 
-  return JsonResponse({'error': 'only POST allowed'})
+  return Response({'error': 'Invalid credentials'}, status=400)
 
-
+@api_view(['POST'])
 def register_view(request):
-  if request.method == 'POST':
-    data = json.loads(request.body)
+  username = request.data.get('username')
+  password = request.data.get('password')
 
-    username = data.get('username')
-    password = data.get('password')
+  if User.objects.filter(username=username).exists():
+    return Response({'error': 'User exists'}, status=400)
 
-    if User.objects.filter(username=username).exists():
-      return JsonResponse({'status': 'exists'})
+  user = User.objects.create_user(username=username, password=password)
+  token = Token.objects.create(user=user)
 
-    User.objects.create_user(username=username, password=password)
-    return JsonResponse({'status': 'ok'})
-
-  return JsonResponse({'error': 'only POST allowed'})
+  return Response({
+    'token': token.key
+  })
